da14a49d3ff889e1909ee8700f9be284fa0184b4
dalia20031994 <eunice.mtz.gz1@gmail.com>
2024-11-30 23:14:21 -0600

    modi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { InicioComponent } from './inicio/inicio.component';
